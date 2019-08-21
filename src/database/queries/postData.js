0e1575c055cdb67a15441c6f16aa8b6f6f5ca90e
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-08-21 11:58:42 +0300

    post Data for users and places Relates #12

---
+++ src/database/queries/postData.js
@@ -0,0 +1,15 @@
+const connection =require('../config/connection');
+
+const postPlace=(placeInfo, userID)=>{
+  return connection.query('insert into places (place_name,location,service,delivery,image,user_id) values ($1,$2,$3,$4,$5,$6)',
+  [placeInfo.placeName,placeInfo.location,placeInfo.service,placeInfo.delivery,placeInfo.img,userID]);
+}
+
+const postUser=(userInfo)=>{
+  return connection.query('insert into users (user_name,email,phone) values ($1,$2,$3) RETURNING *',
+  [userInfo.userName, userInfo.email, userInfo.phone]);
+}
+
+
+module.exports={postPlace,
+                postUser   };
