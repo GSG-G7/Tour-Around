80f954a587f51d3122ca6a504924ce58124430ab
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-08-21 22:27:21 +0300

    added validation for the phone and delivery inputs

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,14 @@
-// const selector = (id) => document.querySelector(`#${id}`);
-// const form = selector('form');
-// const msgSpan = selector('msg');
+const selector = (id) => document.querySelector(`#${id}`);
+const form = selector('form');
+const msgSpan = selector('msg');
 
-// const validate = () => {
-//   let status;
-//   if (selector('delivery').value !== true || selector('delivery').value !== false) {
-//     msgSpan.textContent = 'The delivery filed should have a boolean value';
-//     status = false;
-//   } else if (typeof (selector('phone').value) !== Number) {
-//     msgSpan.textContent = 'the phone should be a number';
-//     status = false;
-//   } else {
-//     status = true;
-//   }
-//   return status;
-// };
-
-// form.addEventListener('onsubmit', (e) => {
-//   if (!validate()) {
-//     e.preventDefault();
-//   }
-// });
+form.addEventListener('submit', (e) => {
+  const messages = [];
+  if (selector('delivery').value !== 'false' && selector('delivery').value !== 'true') {
+    messages.push('The delivery filed should have a boolean value');
+  }
+  if (messages.length > 0) {
+    e.preventDefault();
+    msgSpan.textContent = messages.join(',');
+  }
+});
