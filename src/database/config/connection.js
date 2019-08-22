68c846baca282ec11eb3faa03f2ff24824850e2f
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-08-22 10:20:20 +0300

    fixed

--- src/database/config/connection.js
+++ src/database/config/connection.js
@@ -3,14 +3,15 @@ require('env2')('./config.env');
 
 let DB_URL = '';
 
-if(process.env.NODE_ENV === "test"){
-  DB_URL=process.env.DB_URL_TEST;
-}else if(process.env.NODE_ENV==="production"){
-  DB_URL=process.env.HEROKU_DB_URL;
-}else{
-  DB_URL=process.env.DB_URL;
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'test') {
+  DB_URL = process.env.DB_URL_TEST;
+} else if (process.env.NODE_ENV === 'development') {
+  DB_URL = process.env.DB_URL;
+} else {
+  DB_URL = process.env.HEROKU_DB_URL;
 }
-
+console.log(DB_URL);
 
 const options = {
   connectionString: DB_URL,
